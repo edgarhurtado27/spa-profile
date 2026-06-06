98208d6ab9bc0dc6702e70516cd7192f75f0a755
Edgar Fedora <edgarivancs@gmail.com>
2026-06-05 19:22:30 -0600

    Redesign implemented based on ai propossal

--- src/HeroComponent.tsx
+++ src/HeroComponent.tsx
@@ -186,31 +186,65 @@ function HeroComponent() {
             </p>
 
             <div className="flex gap-9 items-center mt-5 text-gray-700 dark:text-gray-400 mt-2 text-sm">
+              <div className="flex gap-1 items-center align-start">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  className="size-6"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M9 17.25v1.007a3 3 0 0 1-.879 2.122L7.5 21h9l-.621-.621A3 3 0 0 1 15 18.257V17.25m6-12V15a2.25 2.25 0 0 1-2.25 2.25H5.25A2.25 2.25 0 0 1 3 15V5.25m18 0A2.25 2.25 0 0 0 18.75 3H5.25A2.25 2.25 0 0 0 3 5.25m18 0V12a2.25 2.25 0 0 1-2.25 2.25H5.25A2.25 2.25 0 0 1 3 12V5.25"
+                  />
+                </svg>
+                <p>8+Years Experience</p>
+              </div>
 
-            <div className="flex gap-1 items-center align-start">
-<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-6">
-  <path stroke-linecap="round" stroke-linejoin="round" d="M9 17.25v1.007a3 3 0 0 1-.879 2.122L7.5 21h9l-.621-.621A3 3 0 0 1 15 18.257V17.25m6-12V15a2.25 2.25 0 0 1-2.25 2.25H5.25A2.25 2.25 0 0 1 3 15V5.25m18 0A2.25 2.25 0 0 0 18.75 3H5.25A2.25 2.25 0 0 0 3 5.25m18 0V12a2.25 2.25 0 0 1-2.25 2.25H5.25A2.25 2.25 0 0 1 3 12V5.25" />
-</svg>
-              <p>8+Years Experience</p>
-            </div>
+              <div className="flex gap-1 items-center align-start">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  class="size-6"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+                  />
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z"
+                  />
+                </svg>
+                <p>Based in Mexico</p>
+              </div>
 
+              <div className="flex gap-1 items-center align-start">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  class="size-6"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+                  />
+                </svg>
 
-            <div className="flex gap-1 items-center align-start">
-<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-  <path stroke-linecap="round" stroke-linejoin="round" d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
-  <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z" />
-</svg>
-              <p>Based in Mexico</p>
-            </div>
-
-            <div className="flex gap-1 items-center align-start">
-<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-  <path stroke-linecap="round" stroke-linejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
-</svg>
-
-              <p> Available for new oportunities</p>
-            </div>
-
+                <p> Available for new oportunities</p>
+              </div>
             </div>
           </section>
 
@@ -225,70 +259,157 @@ border border-stone-300 dark:border-zinc-800 p-2"
 
         {/* What I Bring! */}
 
+        <div className="mt-15">
+          <h1 className="text-3xl font-bold">What I Bring</h1>
+        </div>
 
         <div
-          className="mt-20 grid
+          className="mt-10 grid
     grid-cols-1
     lg:grid-cols-4
     gap-8"
         >
-
-        <div>backend</div>
-        <div>system design</div>
-        <div>team colaboration</div>
-        <div>continous improvement</div>
-
-        </div>
-
-
-        <div className="mt-5 grid gap-8 lg:grid-cols-[1fr_1fr] lg:items-start">
-          <div className="grid gap-4">
-            <article
-              className="rounded-2xl bg-white dark:bg-[#27282D]
-border border-slate-200 dark:border-zinc-800 p-6 shadow-xl"
+          <div className="flex flex-col gap-3">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              className="size-9 text-[#3B82F6] dark:text-blue-300"
             >
-              <h2 className="text-lg font-semibold">What I Do</h2>
-              <ul className="mt-3 space-y-2 text-sm leading-7 text-gray-700 dark:text-gray-400">
-                {whatIDo.map((item) => (
-                  <li key={item} className="flex gap-2">
-                    <span className="mt-2 h-1.5 w-1.5 rounded-full bg-[#3B82F6] dark:bg-blue-300" />
-                    <span>{item}</span>
-                  </li>
-                ))}
-              </ul>
-            </article>
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M17.25 6.75 22.5 12l-5.25 5.25m-10.5 0L1.5 12l5.25-5.25m7.5-3-4.5 16.5"
+              />
+            </svg>
+            <h2 className="text-lg font-semibold">Backend development</h2>
+
+            <p className="mt-3 text-gray-700 dark:text-gray-400 mt-2 text-sm">
+              Building robuts REST APIs and scalable backend systems with Java.
+            </p>
           </div>
-          <div className="grid gap-4">
-            <article
-              className="rounded-2xl bg-white dark:bg-[#27282D]
-border border-slate-200 dark:border-zinc-800 p-6 shadow-xl"
+
+          <div className="flex flex-col gap-3">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              className="size-9 text-[#3B82F6] dark:text-blue-300"
             >
-              <h2 className="text-lg font-semibold">How I Work</h2>
-              <ul className="mt-3 space-y-2 text-sm leading-7 text-gray-700 dark:text-gray-400">
-                {howIWork.map((item) => (
-                  <li key={item} className="flex gap-2">
-                    <span className="mt-2 h-1.5 w-1.5 rounded-full bg-[#3B82F6] dark:bg-blue-300" />
-                    <span>{item}</span>
-                  </li>
-                ))}
-              </ul>
-            </article>
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M5.25 14.25h13.5m-13.5 0a3 3 0 0 1-3-3m3 3a3 3 0 1 0 0 6h13.5a3 3 0 1 0 0-6m-16.5-3a3 3 0 0 1 3-3h13.5a3 3 0 0 1 3 3m-19.5 0a4.5 4.5 0 0 1 .9-2.7L5.737 5.1a3.375 3.375 0 0 1 2.7-1.35h7.126c1.062 0 2.062.5 2.7 1.35l2.587 3.45a4.5 4.5 0 0 1 .9 2.7m0 0a3 3 0 0 1-3 3m0 3h.008v.008h-.008v-.008Zm0-6h.008v.008h-.008v-.008Zm-3 6h.008v.008h-.008v-.008Zm0-6h.008v.008h-.008v-.008Z"
+              />
+            </svg>
+
+            <h2 className="text-lg font-semibold">System Design</h2>
+
+            <p className="mt-3 text-gray-700 dark:text-gray-400 mt-2 text-sm">
+              Designing distributed systems and database architectures.
+            </p>
+          </div>
+
+          <div className="flex flex-col gap-3">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              className="size-9 text-[#3B82F6] dark:text-blue-300"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M18 18.72a9.094 9.094 0 0 0 3.741-.479 3 3 0 0 0-4.682-2.72m.94 3.198.001.031c0 .225-.012.447-.037.666A11.944 11.944 0 0 1 12 21c-2.17 0-4.207-.576-5.963-1.584A6.062 6.062 0 0 1 6 18.719m12 0a5.971 5.971 0 0 0-.941-3.197m0 0A5.995 5.995 0 0 0 12 12.75a5.995 5.995 0 0 0-5.058 2.772m0 0a3 3 0 0 0-4.681 2.72 8.986 8.986 0 0 0 3.74.477m.94-3.197a5.971 5.971 0 0 0-.94 3.197M15 6.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Zm6 3a2.25 2.25 0 1 1-4.5 0 2.25 2.25 0 0 1 4.5 0Zm-13.5 0a2.25 2.25 0 1 1-4.5 0 2.25 2.25 0 0 1 4.5 0Z"
+              />
+            </svg>
+
+            <h2 className="text-lg font-semibold">Team Collaboration</h2>
+
+            <p className="mt-3 text-gray-700 dark:text-gray-400 mt-2 text-sm">
+              Working closely with cross-functional teams to deliver impactful
+              solutions.
+            </p>
+          </div>
+
+          <div className="flex flex-col gap-3">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              className="size-9 text-[#3B82F6] dark:text-blue-300"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M3.75 3v11.25A2.25 2.25 0 0 0 6 16.5h2.25M3.75 3h-1.5m1.5 0h16.5m0 0h1.5m-1.5 0v11.25A2.25 2.25 0 0 1 18 16.5h-2.25m-7.5 0h7.5m-7.5 0-1 3m8.5-3 1 3m0 0 .5 1.5m-.5-1.5h-9.5m0 0-.5 1.5m.75-9 3-3 2.148 2.148A12.061 12.061 0 0 1 16.5 7.605"
+              />
+            </svg>
+
+            <h2 className="text-lg font-semibold">Continous Improvement</h2>
+
+            <p className="mt-3 text-gray-700 dark:text-gray-400 mt-2 text-sm">
+              Passioante about learning, automation and delivering high-quality
+              code.
+            </p>
           </div>
         </div>
 
         {/* Stack */}
-        <div className="mt-8 rounded-2xl bg-white dark:bg-[#27282D] rounded-lg px-6 py-8 ring shadow-xl ring-gray-900/5 p-6">
-          <h2 className="text-lg font-semibold">My Stack</h2>
-          <div className="mt-4 flex flex-wrap gap-3">
-            {coreStack.map((stackItem) => (
-              <span
-                key={stackItem}
-                className="rounded-full bg-[#34363F] px-4 py-2 text-sm text-slate-100"
-              >
-                {stackItem}
-              </span>
-            ))}
+
+        <div className="mt-15">
+          <h1 className="text-3xl font-bold">Tech Stack</h1>
+        </div>
+
+        <div className="mt-10 grid grid-row gap-5">
+          <div
+            className="grid grid-cols-1
+    lg:grid-cols-[1fr_4fr]
+    gap-8"
+          >
+            <div className="text-md font-bold tracking-widest text-[#3B82F6] dark:text-blue-300">
+              Core expertise
+            </div>
+            <div className="flex gap-1">
+              {coreStack.map((stackItem) => (
+                <span
+                  key={stackItem}
+                  className="rounded-full bg-[#34363F] px-4 py-2 text-sm text-slate-100"
+                >
+                  {stackItem}
+                </span>
+              ))}
+            </div>
           </div>
+
+
+          <div
+            className="grid grid-cols-1
+    lg:grid-cols-[1fr_4fr]
+    gap-8">
+        <div className="text-md font-bold tracking-widest text-[#3B82F6] dark:text-blue-300">
+        Also worked with 
+        </div>
+        <div className="flex gap-1">
+          {coreStack.map((stackItem) => (
+            <span
+              key={stackItem}
+              className="rounded-full bg-[#34363F] px-4 py-2 text-sm text-slate-100"
+            >
+              {stackItem}
+            </span>
+          ))}
+        </div>
+        </div>
+
         </div>
       </div>
     </section>
